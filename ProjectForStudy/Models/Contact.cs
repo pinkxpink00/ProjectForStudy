70950141814760a2c22e1336b2e2382a9ca434d0
pinkxpink <kyperu07@gmail.com>
2024-02-17 22:53:37 +0200

    added Contact Model

---
+++ ProjectForStudy/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectForStudy.Models
+{
+    [Serializable]
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
