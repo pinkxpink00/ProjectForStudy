672f600340635933ade68ddabfe3cd270f48a681
pinkxpink <kyperu07@gmail.com>
2024-02-19 23:27:08 +0200

    refactor code

--- ProjectForStudy/Models/Contact.cs
+++ ProjectForStudy/Models/Contact.cs
@@ -5,6 +5,7 @@ namespace ProjectForStudy.Models
     [Serializable]
     public class Contact
     {
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
