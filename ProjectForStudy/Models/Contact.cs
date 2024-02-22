c5724a6fe5f32577053351b2168cc77e8e113595
pinkxpink <kyperu07@gmail.com>
2024-02-22 21:33:31 +0200

    add razor render component

--- ProjectForStudy/Models/Contact.cs
+++ ProjectForStudy/Models/Contact.cs
@@ -5,7 +5,6 @@ namespace ProjectForStudy.Models
     [Serializable]
     public class Contact
     {
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
