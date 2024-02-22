3efb0562d35d56dded624b52ce972acb9d183280
pinkxpink <kyperu07@gmail.com>
2024-02-22 22:46:51 +0200

    refactored code

---
+++ ProjectForStudy/Components/Pages/Home.razor.cs
@@ -0,0 +1,42 @@
+﻿using ProjectForStudy.Models;
+
+namespace ProjectForStudy.Components.Pages
+{
+    public partial class Home
+    {
+
+        private List<Contact> contacts;
+
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(5000);
+
+            contacts = new List<Contact>
+            {
+            new Contact
+            {
+                FirstName = "German",
+                LastName = "Gritsenko2",
+                Email = "SomeEmail2@gmail.com",
+                Phone = "123123"
+            },
+            new Contact
+            {
+                FirstName = "German",
+                LastName = "Gritsenko1",
+                Email = "SomeEmail1@gmail.com",
+                Phone = "123123"
+            },
+            new Contact
+            {
+                FirstName = "German",
+                LastName = "Gritsenko3",
+                Email = "SomeEmail3@gmail.com",
+                Phone = "123123"
+            }
+            };
+
+            base.OnInitializedAsync();
+        }
+    }
+}
--- ProjectForStudy/Models/Contact.cs
+++ ProjectForStudy/Models/Contact.cs
@@ -8,5 +8,6 @@ namespace ProjectForStudy.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
