c22cf66df4a95f6a3d71782ba4ecd7119608276c
pinkxpink <kyperu07@gmail.com>
2024-02-23 19:46:08 +0200

    learning DI(Dependency Injection) firstly im added AddSindleton<> object in Program.cs

---
+++ ProjectForStudy/ContactService.cs
@@ -0,0 +1,15 @@
+﻿using ProjectForStudy.Models;
+
+namespace ProjectForStudy
+{
+    public class ContactService
+    {
+        public List<Contact> ContactList = new List<Contact>;
+
+        public AddContact(Contact contact)
+        {
+            ContactList.Add(contact);
+        }
+
+    }
+}
--- ProjectForStudy/Program.cs
+++ ProjectForStudy/Program.cs
@@ -1,3 +1,4 @@
+using ProjectForStudy;
 using ProjectForStudy.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddSingleton<ContactService>();
 
 var app = builder.Build();
 
